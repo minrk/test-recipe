596c35aab8bcc0881656c0cf073f76156d3156a7
Min RK <benjaminrk@gmail.com>
2025-03-10 11:11:58 +0100

    relink test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,15 @@
-touch ignored
-touch included
+LIBNAME=test_intermediate
+LIB=lib${LIBNAME}${SHLIB_EXT}
+$CC $CFLAGS $LDFLAGS -shared ${RECIPE_DIR}/a.c -o $LIB
+$CC $CFLAGS $LDFLAGS ${RECIPE_DIR}/b.c -L$PWD -l${LIBNAME} -o b
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+
+cp -v $LIB $PREFIX/lib/
+cp -v b $PREFIX/bin/b
+
+# run test
+otool -L $PREFIX/lib/$LIB
+otool -L $PREFIX/bin/b
+$PREFIX/bin/b
+
